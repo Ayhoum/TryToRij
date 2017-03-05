9dbb7c2b8d528d9b8adf2c3ccf81c3e7fcd1852e
Samobey <suleiman.obied@gmail.com>
2017-03-05 16:43:10 +0100

    new page

---
+++ After_LogIn.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html><!-- -->
+<html lang="en">
+<head>
+    <title>TryToRij</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Lib/style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/droid-arabic-kufi" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css?family=Arsenal" rel="stylesheet">
+    <link rel="stylesheet" href="./Lib/css/font-awesome.min.css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+</head>
+
+
+
+
+<!--    BODY          -->
+
+<body id="TheBody">
+
+<div class="container" id="back-con">
+
+    <nav class="navbar navbar-inverse" role="navigation">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="./"><img src="img/logo250borwhite.png"/>Try To Rij</a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-left">
+                <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> سلة المشتريات</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="caret"></span> حسابي</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#"><span class="glyphicon glyphicon-pencil"></span> حساب جديد</a></li>
+                        <li><a href="#"><span class="glyphicon glyphicon-user"></span> تسجيل الدخول</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#contact"><span class="glyphicon glyphicon-question-sign"></span> سؤال وجواب</a></li>
+                <li><a href="#contact"><span class="glyphicon glyphicon-ok"></span> الإمتحانات</a></li>
+                <li><a href="#about"><span class="glyphicon glyphicon-book"></span> الدروس</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
+                                <!-- Custom content-->
+                         
+<div class="container"> 
+<div class="row">
+  <div class="col-md-12">
+  <div class="col-md-4"> 
+    <div class="thumbnail">
+     <h3>المنهاج النظري لتعليم السياقه</h3>
+      <img src="img/Best-Keyboard-for-Fast-Typing.jpg" alt="lessen">
+      <div class="caption">
+        <p>  الكثير من األمور التي يجب أن تفكر فيها
+كي تحصل على أفضل تعليم ممكن وتصبح سائقا
+يقود بطريقة آمنة ومحافظة على البيئة. ابحث
+عن المعلومات، وتحدث مع المشرف عليك أو
+مدارس القيادة أو أي جهة تعليمية معتمدة أخرى
+)ينطبق على الفئة AM )وقارن بين مختلف
+الطرق التعليمية!
+ّ</p>
+        <p><a href="#" class="btn btn-primary" role="button">تابع</a></p>
+      </div>
+    </div>
+  </div>
+  
+<div class="col-md-4">
+ <div class="thumbnail">
+       <h3>الامتحان النظري للسواقة</h3>
+      <img src="img/road-signs.png" alt="toetsen">
+      <div class="caption"> 
+        <p>
+تم تطوير البرنامج لكي يشابه الطريقة التي يتم بها تقديم الامتحان والتدريب على كيفيه الاجابه على الاسئله الت
+            يتم طرحها وهي تقارب الاسئلة في الامتحان النظري لكن هذا لايعني بالضرورة انها نفس الاسئلة تماما
+ يمكن الدخول الى الرابط لمراجعة المواد.</p>
+        <p><a href="#" class="btn btn-primary" role="button">أبدا الاختبار</a></p>
+      </div>
+    </div>
+</div>
+<div class="col-md-4">
+    <div class="thumbnail">
+        <h3>تطوير الحساب</h3>
+      <img src="img/time-to-upgrade.jpg" alt="upgrade">
+      <div class="caption">
+       
+        <p>فهناك الكثير من الأشياء التي يمكنك تعلّمها بنفسك في هذه الأيام، وخاصةً إذا كان لديك وصول للإنترنت. فلا يتوجّب عليك شراء معدات غالية، 
+          وكثافة ومواصفات وخواص أو التسجيل في دورات مكلفة لتعلم مهارات جديدة يمكن أن تكون ثمينة جداً لمساعدتك في الامتحان النظري للسواقي    .</p>
+        <p><a href="#" class="btn btn-primary" role="button">طور الحساب</a></p>
+      </div>
+    </div>
+ </div>
+ </div>
+</div>
+      
+</div>
+  
+  <!--blog-->
+  
+  <div class="container">
+   <div class="span8">
+    <h1>سؤال عن السرعه القصوى</h1>
+    <p>يوجد حد أعلى للسرعة القصوى التي يجب أن يلتزم بها السائقون عند قيادة سياراتهم في مختلف أنواع الطرق (سواء في داخل المدن أو خارجها) وفقا لموقع وكثافة ومواصفات وخواص كل طريق من هذه الطرق. موضوع حديثنا اليوم سيركّز 
+    على السرعة القصوى التي تفرضها الدول المختلفة على الطرق السريعة في أنحاء العالم لنتعرف على ما هي الوسيلة المثلى التي يمكن استخدامها لمراقبة الطرق ف.</p>
+    <div>
+        <span class="badge badge-primery">Posted 2012-08-02 20:47:04</span><div class="pull-right"><a href="#" class="btn btn-success" role="button">اقرأ المزيد</a></div> 
+    <hr>
+    <h1>الوقوف والتوقف العادي والمفاجئ!</h1>
+    <p>
+يقوم الفاحص بمراقبة وتقييم مهارات الوقوف العادي والوقوف المفاجئ ويراقب الفاحص المتقدم للفحص كيف يقوم بتنفيذ المهارة في إجراءات الوقوف العادي إضافة إلى الوقوف المفاجئ حيث يقوم الفاحص فجأة بطلب الوقوف بسرعة وعلى المتقدم للفحص
+ إن يحسسن تطبيق الوقوف وفي حال عدم قيام المتدرب بالعملية حسب المطلوب يمكن إن يقوم الفاحص بإتمام الوقوف بشكل امن  
+
+</p>
+    <div>
+        <span class="badge badge-success">Posted 2012-08-02 20:47:04</span><div class="pull-right"><a href="#" class="btn btn-success" role="button">اقرأ المزيد</a></div> 
+    </div>     
+    <hr>
+</div>
+</div>
+  <div class="container">
+    <div class="row-fluid">
+     <div class="col-md-9">
+         <div class="col-md-3">
+              <ul class="thumbnails">
+            <div style="padding:4px">
+              <img alt="300x200" style="max-width: 100%" src="img/user-male-shape-in-a-circle--ios-7-interface-symbol_318-35357.png">
+            </div>
+            
+         </div>
+     
+         <div class="col-md-5">
+              <ul class="thumbnails">
+            <div class="span4">
+              <h4>معدل التطور اليومي</h4>
+                <div class="progress progress-striped active">
+                 <div class="bar btn btn-success" style="width:80%"></div>
+               </div>
+                <p>
+                 <button class="btn btn-primary" type="button">تطوير الحساب</button>
+                 <button class="btn btn-success" type="button">شراء الامتحان</button>
+               </p>
+              </div>
+            </div>
+             </ul>
+             </div>
+          
+
+     
+      </div>
+    </div>
+</div>
+<!--            FOOTER            -->
+
+<footer id="footer1" class="container-fluid text-center">
+    <a href="#TheBody" title="To Top">
+        <span class="glyphicon glyphicon-chevron-up"></span>
+    </a>
+    <p style="direction: rtl">تم تطوير الموقع من قبل: <a href="#">SAA</a> .جميع الحقوق محفوظة لشركة SAA</p><br/><p style="direction: rtl">&copy;2017</p>
+</footer>
+
+</body>
+</html>
+
+<script>
+
+    $('.count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+</script>
